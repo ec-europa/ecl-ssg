bb3b085a57df4219445f9faba3faf198ef3c4f49
planctus <davidepuntoferrante@gmail.com>
2026-02-16 15:51:02 +0200

    chore: Re-adding sharp plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -71,6 +71,8 @@ module.exports = {
       },
     },
     `gatsby-plugin-image`,
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
     "gatsby-transformer-remark",
     {
       resolve: 'gatsby-plugin-i18n',
